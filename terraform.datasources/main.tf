426dcaeda9bb30b4771711c716bdc982121f9929
Kartik <kartik@hyperverge.co>
2021-05-30 00:06:57 +0530

    Fix: datasoruces main.tf format

--- terraform.datasources/main.tf
+++ terraform.datasources/main.tf
@@ -1,8 +1,9 @@
 resource "local_file" "pet" {
-    filename = "/Users/kartik/Desktop/projects/terraform/terraform.datasources/root/pets.txt"
-    content = "My pet's name is Romy"
+  filename = "/Users/kartik/Desktop/projects/terraform/terraform.datasources/root/pets.txt"
+  content  = "My pet's name is Romy"
+  # To use data as content use: content = data.local_file.dog.content
 }
 
-data local_file "dog" {
-    filename = "/Users/kartik/Desktop/projects/terraform/terraform.datasources/root/dogs.txt"
+data "local_file" "dog" {
+  filename = "/Users/kartik/Desktop/projects/terraform/terraform.datasources/root/dogs.txt"
 }
