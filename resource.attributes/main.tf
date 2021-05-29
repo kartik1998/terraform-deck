0aad1ea8f98fdbb80279bf1ee46caa53c55d2e6d
Kartik <kartik@hyperverge.co>
2021-05-29 22:52:55 +0530

    Add: resource attributes

--- resource.attributes/main.tf
+++ resource.attributes/main.tf
@@ -1,6 +1,8 @@
 resource "local_file" "pet" {
     filename = var.filename
     content = "My pet's name is ${random_pet.my-pet.id}"
+    # to specify dependency:
+    # depends_on = ["random_pet.my-pet"] # No use to explicitly specify this
 }
 
 resource "random_pet" "my-pet" {
