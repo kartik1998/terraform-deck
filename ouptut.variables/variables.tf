9597a626be1c3260f0c06edf579156687e24c268
Kartik <kartik@hyperverge.co>
2021-05-29 22:59:39 +0530

    Add: output variables

---
+++ ouptut.variables/variables.tf
@@ -0,0 +1,24 @@
+variable "filename" {
+  default = "/Users/kartik/Desktop/projects/terraform/output.variables/root/pets.txt"
+  # if default isn't used after terraform apply is run the input is taken during runtime 
+  # we can also use environment variable as TF_VAR_default
+  # -var flag can also be used to set the variable
+  # automatically loaded varible files by terraform: *.auto.tfvars or *.auto.tfvars.json
+  # You can use -var-file flag with taerraform.tfvars or terraform.tfvars.json
+}
+
+variable "content" {
+    default = "Petname romey"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}
