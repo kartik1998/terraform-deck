adc719a7cde7b3f9fd5350bb7a6927ffeb9fe253
Kartik <kartik@hyperverge.co>
2021-05-29 22:26:35 +0530

    Update: readme

--- input.variables/README.md
+++ input.variables/README.md
@@ -1,3 +1,5 @@
 ### Refer diagram
 
 ![](https://raw.githubusercontent.com/kartik1998/dotterraform/master/images/understanding_variables.png)
+
+![](https://raw.githubusercontent.com/kartik1998/dotterraform/master/images/variable_types.png)
--- input.variables/variables.tf
+++ input.variables/variables.tf
@@ -1,5 +1,10 @@
 variable "filename" {
   default = "/Users/kartik/Desktop/projects/terraform/input.variables/root/pets.txt"
+  # if default isn't used after terraform apply is run the input is taken during runtime 
+  # we can also use environment variable as TF_VAR_default
+  # -var flag can also be used to set the variable
+  # automatically loaded varible files by terraform: *.auto.tfvars or *.auto.tfvars.json
+  # You can use -var-file flag with taerraform.tfvars or terraform.tfvars.json
 }
 
 variable "content" {
