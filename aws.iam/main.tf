9009df90ffb82051f7d122df050fedcffe7c2aef
Kartik <kartik@hyperverge.co>
2021-05-30 20:30:47 +0530

    Fix aws.iam format

--- aws.iam/main.tf
+++ aws.iam/main.tf
@@ -1,20 +1,20 @@
 provider "aws" {
-    region = "us-west-2"
-    access_key = "AKIAEXMPLEKEY"
-    secret_key = "SECRETKEY"
-    # Don't hardcode the access credentials. 
-    # either use aws configure to setup credentials or fetch from env variables. Refer readme
+  region     = "us-west-2"
+  access_key = "AKIAEXMPLEKEY"
+  secret_key = "SECRETKEY"
+  # Don't hardcode the access credentials. 
+  # either use aws configure to setup credentials or fetch from env variables. Refer readme
 }
 resource "aws_iam_user" "admin-user" {
-    name = "kartik"
-    tags = {
-        Description = "Technical Team lead"
-    }
+  name = "kartik"
+  tags = {
+    Description = "Technical Team lead"
+  }
 }
 
 resource "aws_iam_policy" "adminUser" {
-    name = "AdminUsers"
-    policy = <<EOF
+  name   = "AdminUsers"
+  policy = <<EOF
         {
             "Version": "2012-10-17"
             "Statement": [
@@ -29,6 +29,6 @@ resource "aws_iam_policy" "adminUser" {
 }
 
 resource "aws_iam_user_policy_attachment" "kartik-admin-access" {
-    user = aws_iam_user.admin-user.name
-    policy_arn = aws_iam_policy.adminUser.arn
+  user       = aws_iam_user.admin-user.name
+  policy_arn = aws_iam_policy.adminUser.arn
 }
