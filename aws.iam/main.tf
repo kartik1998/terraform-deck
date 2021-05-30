a32267a1df1d4a91e06a4ad944fb055c27278bc8
Kartik <kartik@hyperverge.co>
2021-05-30 20:30:14 +0530

    Add and attach policy for aws iam

--- aws.iam/main.tf
+++ aws.iam/main.tf
@@ -11,3 +11,24 @@ resource "aws_iam_user" "admin-user" {
         Description = "Technical Team lead"
     }
 }
+
+resource "aws_iam_policy" "adminUser" {
+    name = "AdminUsers"
+    policy = <<EOF
+        {
+            "Version": "2012-10-17"
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "*",
+                    "Resource": "*"
+                }
+            ]
+        }
+    EOF
+}
+
+resource "aws_iam_user_policy_attachment" "kartik-admin-access" {
+    user = aws_iam_user.admin-user.name
+    policy_arn = aws_iam_policy.adminUser.arn
+}
