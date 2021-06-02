a2b5cf26ee87d0586fa19d0b6f884746a85ddc2a
Kartik <kartik@hyperverge.co>
2021-06-02 16:32:18 +0530

    Update: create.ec2 directory

--- create.ec2/main.tf
+++ create.ec2/main.tf
@@ -1,4 +1,24 @@
-resource "aws_instance" "webserver" {
-  ami           = var.ami
-  instance_type = var.instance_type
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
--- create.ec2/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ami" {
-  default = "ami-0example43b6fa8922"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
